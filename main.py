b2bd66ca8da09991e846d697c1e0bf7e28c216ee
live4574 <adolfd@naver.com>
2019-01-07 18:11:13 +0900

    image preprocess 331

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def l2_normalize(v):
 def preprocess(queries, db):
     query_img = []
     reference_img = []
-    img_size = (224, 224)
+    img_size = (331, 331)
 
     for img_path in queries:
         img = cv2.imread(img_path, 1)
